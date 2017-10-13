b811c2001cbe52b54585aa04f437036c1419be5b
xupeng <xpbtmusic@163.com>
2017-10-14 00:46:58 +0800

    add shangxiayongshu and dongyao

--- js/Shichen.js
+++ js/Shichen.js
@@ -15,6 +15,69 @@ Shichen.time2shiChen2=function(hour,minute){
 
 	return [shichen,chuZheng,ke];
 }
+Shichen.getHourYongShu=function(hour_string){
+    let hour = Number.parseInt(hour_string,10);
+
+    let tishu;
+     if(hour<59){
+     	hour+=2400;
+     }
+     if(hour<2459&&hour>2300){
+     	tishu=['子',1];
+	 }else if(hour>100&&hour<259){
+     	tishu=['丑',2];
+
+	 }else if(hour>300&&hour<459){
+         tishu=['寅',3];
+
+	 }
+     else if(hour>500&&hour<659){
+         tishu=['卯',4];
+
+     }
+     else if(hour>700&&hour<859){
+         tishu=['辰',5];
+
+     }
+     else if(hour>900&&hour<1059){
+         tishu=['己',6];
+
+     }
+     else if(hour>1100&&hour<1259){
+         tishu=['午',7];
+
+     }
+     else if(hour>1300&&hour<1459){
+         tishu=['未',8];
+
+     }
+     else if(hour>1500&&hour<1659){
+         tishu=['申',9];
+
+     }
+     else if(hour>1700&&hour<1859){
+         tishu=['酉',10];
+
+     }
+     else if(hour>1900&&hour<2059){
+         tishu=['戊',11];
+
+     }
+
+     else if(hour>2100&&hour<2259){
+         tishu=['亥',12];
+
+     }
+
+
+
+
+
+
+
+    return tishu;
+}
+
 var time2shiChen = {
 	'23':'子',
 	'0':'子',
@@ -41,6 +104,27 @@ var time2shiChen = {
 	'21':'亥',
 	'22':'亥',
 };
+Shichen.xianTianBagua = {//先天
+    '1':'乾',
+    '2':'兑',
+    '3':'离',
+    '4':'震',
+    '5':'巽',
+    '6':'坎',
+    '7':'艮',
+    '8':'坤'
+};
+Shichen.houTianBagua = {//后天
+    '1':'坎',
+    '2':'坤',
+    '3':'震',
+    '4':'巽',
+    '5':'#',
+    '6':'乾',
+    '7':'兑',
+    '8':'艮',
+	'9':'离'
+};
 Shichen.time2YongShu = {//dizhi
     '子':1,
     '丑':2,
@@ -55,5 +139,7 @@ Shichen.time2YongShu = {//dizhi
     '戌':11,
     '亥':12,
 };
+
+
 Shichen.time2TianganYongShu = {'甲':1, '乙':2, '丙':3, '丁':3, '戊':4, '己':5, '庚':6, '辛':7, '壬':9, '癸':10}, //天干
   module.exports=Shichen;
